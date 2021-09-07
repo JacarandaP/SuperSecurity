99517f62e4ea166d79e84b09f0244a83d212385e
Jacaranda Perez <jacaranda.perez@gmail.com>
2021-09-07 14:51:08 +0200

    cusumer entity createdt

---
+++ src/main/java/spring/security/domain/Customer.java
@@ -0,0 +1,27 @@
+package spring.security.domain;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * Created by Jacaranda Perez
+ * Date: 2021-09-07
+ * Project: Security
+ */
+@Data
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String username;
+    private String password;
+    private String roleType;
+
+}
